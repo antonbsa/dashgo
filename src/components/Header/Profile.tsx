de8f299af9de5204f5550988ffde33d3881e4732
Anton B <antonbsa@gmail.com>
2022-04-22 15:24:01 -0300

    make Header responsive

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,17 +1,24 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Anton</Text>
-        <Text
-          color="gray.300"
-          fontSize="small"
-        >
-          antonbsa@gmail.com
-        </Text>
-      </Box>
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Anton</Text>
+          <Text
+            color="gray.300"
+            fontSize="small"
+          >
+            antonbsa@gmail.com
+          </Text>
+        </Box>
+      )}
+
       <Avatar size="md" name="Anton Barboza" src="https://github.com/antonbsa.png" />
     </Flex>
   )
