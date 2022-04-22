3e3c84104651259d3e7c6bcfa9b85dc30f191470
Anton B <antonbsa@gmail.com>
2022-04-22 16:06:27 -0300

    make Dashboard, User list and User create responsive

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,4 +1,4 @@
-import { Box, Button, Checkbox, Flex, Heading, Icon, Table, Tbody, Td, Text, Th, Thead, Tr } from "@chakra-ui/react";
+import { Box, Button, Checkbox, Flex, Heading, Icon, IconButton, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpoint, useBreakpointValue } from "@chakra-ui/react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
@@ -6,6 +6,11 @@ import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  })
+
   return (
     <Box>
       <Header />
@@ -31,17 +36,17 @@ export default function UserList() {
           <Table colorScheme="whiteAlpha" >
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8">
+                <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                <Th>Data de cadastro</Th>
+                {isWideVersion && <Th>Data de cadastro</Th>}
                 <Th width="8"></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6" >
+                <Td px={["4", "4", "6"]} >
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
@@ -50,16 +55,16 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.3001">antonbsa@gmail.com</Text>
                   </Box>
                 </Td>
-                <Td>04 de Abril, 2021</Td>
+                {isWideVersion && <Td>04 de Abril, 2021</Td>}
                 <Td>
                   <Button
                     as="a"
                     size="sm"
                     fontSize="sm"
                     colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
                   >
-                    Editar
+                    <Icon aria-label="Editar usuário" as={RiPencilLine} fontSize="16" />
+                    {isWideVersion ? 'Editar' : ''}
                   </Button>
                 </Td>
               </Tr>
